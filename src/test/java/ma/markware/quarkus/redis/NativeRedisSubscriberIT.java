e5207201fdb41b2789673cd2adcb7f590764ceae
Oussama Markad <markad.oussama@gmail.com>
2020-12-16 21:20:50 +0100

    Delete tests

--- src/test/java/ma/markware/quarkus/redis/NativeRedisSubscriberIT.java
+++
@@ -1,9 +0,0 @@
-package ma.markware.quarkus.redis;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeRedisSubscriberIT extends RedisSubscriberTest {
-
-    // Execute the same tests but in native mode.
-}
